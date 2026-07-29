8be612f9062cd395c485e8abf904038ab77d7a1e
Anthony <anthonydistefano.sc@gmail.com>
2026-07-28 20:55:43 -0600

    avances

--- resources/views/navegacion/navbar.blade.php
+++ resources/views/navegacion/navbar.blade.php
@@ -1,4 +1,5 @@
-  @vite(['resources/css/navbar.css'])
+  @vite(['resources/css/navbar.css', 'resources/js/navbar.js'])
+
   <link
       href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&display=swap"
       rel="stylesheet" />
